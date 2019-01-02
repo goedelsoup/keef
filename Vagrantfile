79cdcd48840c261b868001ad2da37d47bf8ee44b
Cory Parent <goedelsoup@gmail.com>
2019-01-02 10:19:57 -0500

    support more flexible topologies

--- Vagrantfile
+++ Vagrantfile
@@ -20,9 +20,12 @@ topology = make_topology(datacenters, {
 })
 
 inventory = topology['inventory']
-port_map = topology['port_map']
+host_map = topology['host_map']
+
+# puts topology.to_yaml
 
 File.open('./inventory.yml', 'w') { |file| file.write(inventory.to_yaml) }
+File.open('./host_map.yml', 'w') { |file| file.write(host_map.to_yaml) }
 
 def make_host_name(datacenter, service, index)
     "#{service}-#{index}.#{datacenter}.consul"
@@ -45,6 +48,7 @@ consul_group = []
 vault_group = []
 nomad_group = []
 traefik_group = []
+registry_group = []
 
 host_map = {}
 
@@ -209,23 +213,35 @@ datacenters.each_with_index do |(datacenter, dc_config), index|
             "traefik_template" => "./templates/traefik-consul.toml"
         }]
     end
+
+    # Add Docker registry
+    if dc_config.key?("registry")
+        registry_host_id = "registry-#{datacenter}"
+        registry_host = make_host_name(datacenter, "registry", 1)
+        registry_ip = make_private_ip(dc_config["cidr_segment"], dc_config["registry"], 1)
+
+        consul_group << registry_host_id
+        registry_group << registry_host_id
+        host_map.merge! Hash[registry_ip, registry_host]
+
+        host_vars.merge! Hash[registry_host_id, {
+            "consul_node_role" => "client",
+            "consul_datacenter" => datacenter,
+            "consul_install_dependencies" => false,
+            "consul_bind_address" => registry_ip,
+            "consul_dnsmasq_enable" => true,
+            "consul_raw_key" => consul_raw_key,
+        }]
+    end
 end
 
-ansible_groups = {
-    "consul_instances"  => consul_group,
-    "vault_instances"   => vault_group,
-    "nomad_instances"   => nomad_group,
-    "traefik_instances" => traefik_group
-}
-
-# puts ansible_groups
-# puts JSON.pretty_generate(host_vars)
+# puts host_vars.to_yaml
 
 Vagrant.configure("2") do |config|
 
     config.vm.provider "virtualbox" do |v|
-        v.memory = 512
-        v.cpus = 1
+        v.memory = 1500
+        v.cpus = 2
     end
 
     # Loop all defined datacenters and provision a Hashicorp cluster
@@ -233,57 +249,61 @@ Vagrant.configure("2") do |config|
         
         # Configure Consul server nodes
         (1..dc_config["consul"]["count"]).each do |consul_index|
+            
             config.vm.define "consul-#{datacenter}-#{consul_index}" do |consul|
-                private_ip = make_private_ip(dc_config["cidr_segment"], dc_config["consul"], consul_index)
+            private_ip = make_private_ip(dc_config["cidr_segment"], dc_config["consul"], consul_index)
 
-                consul.vm.box = "centos/7"
-                consul.vm.hostname = make_host_name(datacenter, "consul", consul_index)
-                consul.vm.network "private_network", ip: private_ip
+            consul.vm.box = "centos/7"
+            consul.vm.hostname = make_host_name(datacenter, "consul", consul_index)
+            consul.vm.network "private_network", ip: private_ip
 
-                if index == 1
-                    traefik.vm.network "forwarded_port", guest: 8500, host: 8500
-                end
+            if (!dc_config.key?("traefik") && consul_index == 1 && index == 1)
+                consul.vm.network "forwarded_port", guest: 80, host: 80
+                consul.vm.network "forwarded_port", guest: 8080, host: 8080
+                consul.vm.network "forwarded_port", guest: 443, host: 443
+            end
 
-                add_all_hosts(consul, host_map)
+            add_all_hosts(consul, host_map)
             end
         end
 
         # Configure Vault
         if (dc_config.key?("vault"))
+
             config.vm.define "vault-#{datacenter}" do |vault|
-                private_ip = make_private_ip(dc_config["cidr_segment"], dc_config["vault"], 1)
+            private_ip = make_private_ip(dc_config["cidr_segment"], dc_config["vault"], 1)
 
-                vault.vm.box = "centos/7"
-                vault.vm.hostname = make_host_name(datacenter, "vault", 1)
-                vault.vm.network "private_network", ip: private_ip
+            vault.vm.box = "centos/7"
+            vault.vm.hostname = make_host_name(datacenter, "vault", 1)
+            vault.vm.network "private_network", ip: private_ip
 
-                add_all_hosts(vault, host_map)
+            add_all_hosts(vault, host_map)
             end
         end
 
+        # Configure Nomad server nodes
         if dc_config.key?('nomad_server')
-            # Configure Nomad server nodes
+
             (1..dc_config["nomad_server"]["count"]).each do |nomad_index|
 
                 config.vm.define "nomad-#{datacenter}-#{nomad_index}" do |nomad|
-                    private_ip = make_private_ip(dc_config["cidr_segment"], dc_config["nomad_server"], nomad_index)
+                private_ip = make_private_ip(dc_config["cidr_segment"], dc_config["nomad_server"], nomad_index)
 
-                    nomad.vm.box = "centos/7"
-                    nomad.vm.hostname = make_host_name(datacenter, "nomad", nomad_index)
-                    nomad.vm.network "private_network", ip: private_ip
+                nomad.vm.box = "centos/7"
+                nomad.vm.hostname = make_host_name(datacenter, "nomad", nomad_index)
+                nomad.vm.network "private_network", ip: private_ip
 
-                    nomad.vm.memory = 1024
-                    nomad.vm.cpus = 2
-
-                    add_all_hosts(nomad, host_map)
+                add_all_hosts(nomad, host_map)
                 end
             end
         end
 
         # Configure Nomad client nodes
-        (1..dc_config["nomad_client"]["count"]).each do |nomad_index|
+        if dc_config.key?('nomad_client')
 
-            config.vm.define "nomad-client-#{datacenter}-#{nomad_index}" do |nomad|
+            (1..dc_config["nomad_client"]["count"]).each do |nomad_index|
+
+                config.vm.define "nomad-client-#{datacenter}-#{nomad_index}" do |nomad|
                 private_ip = make_private_ip(dc_config["cidr_segment"], dc_config["nomad_client"], nomad_index)
 
                 nomad.vm.box = "centos/7"
@@ -291,58 +311,42 @@ Vagrant.configure("2") do |config|
                 nomad.vm.network "private_network", ip: private_ip
 
                 add_all_hosts(nomad, host_map)
+                end
             end
         end
 
         # Configure Traefik
         if dc_config.key?("traefik")
+
             config.vm.define "traefik-#{datacenter}" do |traefik|
-                private_ip = make_private_ip(dc_config["cidr_segment"], dc_config["traefik"], 1)
+            private_ip = make_private_ip(dc_config["cidr_segment"], dc_config["traefik"], 1)
 
-                traefik.vm.box = "centos/7"
-                traefik.vm.hostname = make_host_name(datacenter, "traefik", 1)
-                traefik.vm.network "private_network", ip: private_ip
+            traefik.vm.box = "centos/7"
+            traefik.vm.hostname = make_host_name(datacenter, "traefik", 1)
+            traefik.vm.network "private_network", ip: private_ip
 
-                traefik.vm.network "forwarded_port", guest: 80, host: 80
-                traefik.vm.network "forwarded_port", guest: 8080, host: 8080
-                traefik.vm.network "forwarded_port", guest: 443, host: 443
+            traefik.vm.network "forwarded_port", guest: 80, host: 80
+            traefik.vm.network "forwarded_port", guest: 8080, host: 8080
+            traefik.vm.network "forwarded_port", guest: 443, host: 443
+            # traefik.vm.network "forwarded_port", guest: 53, host: 53, protocol: 'tcp'
+            # traefik.vm.network "forwarded_port", guest: 53, host: 53, protocol: 'udp'
 
-                add_all_hosts(traefik, host_map)
+            add_all_hosts(traefik, host_map)
             end
         end
 
-        config.vm.provision "all", type: "ansible" do |ansible|
-            ansible.compatibility_mode = "2.0"
-            ansible.playbook = "ansible/main.yml"
-            ansible.inventory_path = './inventory.yml'
+        # Configure Docker registry
+        if dc_config.key?("registry")
+
+            config.vm.define "registry-#{datacenter}" do |registry|
+            private_ip = make_private_ip(dc_config["cidr_segment"], dc_config["registry"], 1)
+
+            registry.vm.box = "centos/7"
+            registry.vm.hostname = make_host_name(datacenter, "registry", 1)
+            registry.vm.network "private_network", ip: private_ip
+            
+            add_all_hosts(registry, host_map)
+            end
         end
-
-        # config.vm.provision "consul", type: "ansible" do |ansible|
-        #     ansible.compatibility_mode = "2.0"
-        #     ansible.playbook = "ansible/consul.yml"
-        #     ansible.groups = ansible_groups
-        #     ansible.host_vars = host_vars
-        # end
-
-        # config.vm.provision "vault", type: "ansible" do |ansible|
-        #     ansible.compatibility_mode = "2.0"
-        #     ansible.playbook = "ansible/vault.yml"
-        #     ansible.groups = ansible_groups
-        #     ansible.host_vars = host_vars
-        # end
-
-        # config.vm.provision "nomad", type: "ansible" do |ansible|
-        #     ansible.compatibility_mode = "2.0"
-        #     ansible.playbook = "ansible/nomad.yml"
-        #     ansible.groups = ansible_groups
-        #     ansible.host_vars = host_vars
-        # end
-
-        # config.vm.provision "traefik", type: "ansible" do |ansible|
-        #     ansible.compatibility_mode = "2.0"
-        #     ansible.playbook = "ansible/traefik.yml"
-        #     ansible.groups = ansible_groups
-        #     ansible.host_vars = host_vars
-        # end
     end
 end
--- lib/inventory.rb
+++ lib/inventory.rb
@@ -2,46 +2,66 @@
 def make_topology(datacenters, config)
     {
         'inventory' => make_inventory(datacenters, config),
-        'port_map'  => make_port_map(datacenters, config)
+        'host_map'  => make_host_map(datacenters, config)
     }
 end
 
 def make_inventory(datacenters, config)
 
-    def defaults(h, config)
+    def provisioner_host_vars(h)
         {
             'ansible_host' => h['ansible_host'],
+            'ansible_ssh_private_key_file' => h['ansible_ssh_private_key_file']
+        }
+    end
+
+    def provisioner_group_vars(config)
+        {
             'ansible_user' => 'vagrant',
-            'ansible_ssh_private_key_file' => h['ansible_ssh_private_key_file'],
             'ansible_ssh_extra_args' => '-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null',
-            'consul_raw_key' => config['consul_raw_key']
+            'consul_raw_key' => config['consul_raw_key'],
         }
     end
 
     {
         'consul' => {
             'hosts' => get_hosts(datacenters, 'consul').map do |h|
-                [h['inventory_name'], h['vars'].merge(defaults(h, config))]
+                [h['inventory_name'], h['vars'].merge(provisioner_host_vars(h))]
             end.to_h,
             'vars'  => {
                 'consul_install_dependencies' => false,
                 'consul_dnsmasq_enable' => true,
                 'consul_group_name' => 'consul',
-            }
+                'consul_client_address' => '127.0.0.1',
+                'consul_addresses' => {
+                    'dns' => '0.0.0.0',
+                    'http' => '0.0.0.0',
+                    'https' => '0.0.0.0',
+                    'rpc' => '0.0.0.0',
+                    'grpc' => '0.0.0.0',
+                },
+                'consul_config_custom' => {
+                    'telemetry' => {
+                        'prometheus_retention_time' => '744h'
+                    }
+                }
+            }.merge(provisioner_group_vars(config))
         },
         'vault' => {
             'hosts' => get_hosts(datacenters, 'vault').map do |h|
-                [h['inventory_name'], h['vars'].merge(defaults(h, config))]
+                [h['inventory_name'], h['vars'].merge(provisioner_host_vars(h))]
             end.to_h,
             'vars'  => {
                 'vault_ui' => true,
                 'vault_iface' => 'eth0',
                 'vault_group_name' => 'vault',
-            }
+                'vault_cluster_disable' => false,
+                'vault_tls_disable' => true
+            }.merge(provisioner_group_vars(config))
         },
         'nomad' => {
             'hosts' => get_hosts(datacenters, 'nomad').map do |h|
-                [h['inventory_name'], h['vars'].merge(defaults(h, config))]
+                [h['inventory_name'], h['vars'].merge(provisioner_host_vars(h))]
             end.to_h,
             'vars' => {
                 'nomad_ports_http' => 4646,
@@ -49,20 +69,40 @@ def make_inventory(datacenters, config)
                 'nomad_ports_serf' => 4648,
                 'nomad_use_consul' => true,
                 'nomad_group_name' => 'nomad',
-            }
+                'nomad_docker_enable' => true,
+                'nomad_vault_enabled' => false,
+                'nomad_vault_address' => 'vault.service.consul:8200',
+                'nomad_network_interface' => 'eth1',
+                'nomad_config_custom' => {
+                    'telemetry' => {
+                        'collection_interval' => '1s',
+                        'disable_hostname' => true,
+                        'prometheus_metrics' => true,
+                        'publish_allocation_metrics' => true,
+                        'publish_node_metrics' => true
+                    }
+                },
+            }.merge(provisioner_group_vars(config))
         },
         'traefik' => {
             'hosts' => get_hosts(datacenters, 'traefik').map do |h|
-                [h['inventory_name'], h['vars'].merge(defaults(h, config))]
+                [h['inventory_name'], h['vars'].merge(provisioner_host_vars(h))]
             end.to_h,
             'vars'  => {
-                'traefik_template' => './templates/traefik-consul.toml'
-            }
+                # 'consul_address' => '0.0.0.0',
+                'traefik_template' => './templates/traefik-consul.toml',
+            }.merge(provisioner_group_vars(config))
+        },
+        'registry' => {
+            'hosts' => get_hosts(datacenters, 'registry').map do |h|
+                [h['inventory_name'], h['vars'].merge(provisioner_host_vars(h))]
+            end.to_h,
+            'vars'  => {}.merge(provisioner_group_vars(config))
         }
     }
 end
 
-def make_port_map(datacenters, config)
+def make_host_map(datacenters, config)
     consul = get_hosts(datacenters, 'consul').map do |h|
         [h['ip_addr'], h['host']]
     end
@@ -75,9 +115,16 @@ def make_port_map(datacenters, config)
     traefik = get_hosts(datacenters, 'traefik').map do |h|
         [h['ip_addr'], h['host']]
     end
+    registry = get_hosts(datacenters, 'registry').map do |h|
+        [h['ip_addr'], h['host']]
+    end
 
-    consul.concat(vault).concat(nomad).concat(traefik).to_h
+    consul.concat(vault).concat(nomad).concat(traefik).concat(registry).to_h
 end
+
+# def make_vm_cluster(datacenters, config)
+
+# end
 
 def get_hosts(datacenters, service)
     datacenters.map do |(datacenter_name, datacenter_config)|
@@ -108,7 +155,11 @@ def make_private_ip(base, service, index)
 end
 
 def make_host_name(datacenter, service, index)
-    "#{service}-#{index}.#{datacenter}.consul"
+    if !(index == nil)
+        "#{service}-#{index}.#{datacenter}.consul".gsub '_', '-'
+    else
+        "#{service}-1.#{datacenter}.consul".gsub '_', '-'
+    end
 end
 
 def make_inventory_name(datacenter, service, index)
@@ -117,6 +168,15 @@ def make_inventory_name(datacenter, service, index)
     else
         "#{service}-#{datacenter}".gsub '_', '-'
     end
+end
+
+def make_network(datacenter, datacenter_config, service, service_config, index)
+    cidr_ii = datacenter_config['cidr_segment']
+    {
+        'hostname' => make_host_name(datacenter, service, index),
+        'ip_address' => make_private_ip(cidr_ii, service_config, index),
+        'inventory_name' => make_inventory_name(datacenter, service, index)
+    }
 end
 
 def make_service(service, datacenter, datacenter_config, service_name, service_config, index)
@@ -129,71 +189,83 @@ def make_service(service, datacenter, datacenter_config, service_name, service_c
         make_nomad_service(datacenter, datacenter_config, service_name, service_config, index)
     when "traefik"
         make_traefik_service(datacenter, datacenter_config, service_name, service_config, index)
+    when "registry"
+        make_registry_service(datacenter, datacenter_config, service_name, service_config, index)
     end
+end
+
+def is_consul_bootstrap(service, index)
+    (service == 'consul' && index == 1)
 end
 
 def make_consul_service(datacenter, datacenter_config, service, service_config, index)
 
-    cidr_ii = datacenter_config['cidr_segment']
-
-    hostname = make_host_name(datacenter, service, index)
-    ip_address = make_private_ip(cidr_ii, service_config, index)
-    inventory_name = make_inventory_name(datacenter, service, index)
+    network = make_network(datacenter, datacenter_config, service, service_config, index)
     
     def get_role(service, index)
-        if service == 'consul'
-            if index == 1
-                'bootstrap'
-            else
-                'server'
-            end
+        if is_consul_bootstrap(service, index)
+            'bootstrap'
+        elsif service == 'consul'
+            'server'
         else
             'client'
         end
     end
 
     {
-        "host" => hostname,
-        "ip_addr" => ip_address,
-        "inventory_name" => inventory_name,
+        "host" => network['hostname'],
+        "ip_addr" => network['ip_address'],
+        "inventory_name" => network['inventory_name'],
 
-        "ansible_host" => ip_address,
-        "ansible_ssh_private_key_file" => "./.vagrant/machines/#{inventory_name}/virtualbox/private_key",
+        "ansible_host" => network['ip_address'],
+        "ansible_ssh_private_key_file" => "./.vagrant/machines/#{network['inventory_name']}/virtualbox/private_key",
 
-        "vars" => {
-            "consul_node_role" => get_role(service, index),
-            "consul_datacenter" => datacenter,
-            "consul_install_dependencies" => false,
-            "consul_bind_address" => ip_address,
-            "consul_dnsmasq_enable" => true,
-            "consul_raw_key" => datacenter_config['consul_raw_key']
+        'vars' => {
+            'consul_node_role' => get_role(service, index),
+            'consul_datacenter' => datacenter,
+            'consul_bind_address' => network['ip_address']
         }
     }
 end
 
 def make_vault_service(datacenter, datacenter_config, service, service_config, index)
 
-    cidr_ii = datacenter_config["cidr_segment"]
-
-    hostname = make_host_name(datacenter, service, index)
-    ip_address = make_private_ip(cidr_ii, service_config, index)
-    inventory_name = make_inventory_name(datacenter, service, index)
+    network = make_network(datacenter, datacenter_config, service, service_config, index)
     
-    if service == 'vault'
+    if (service == 'vault')
     {
-        "host" => hostname,
-        "ip_addr" => ip_address,
-        "inventory_name" => inventory_name,
+        "host" => network['hostname'],
+        "ip_addr" => network['ip_address'],
+        "inventory_name" => network['inventory_name'],
 
-        "ansible_host" => ip_address,
-        "ansible_ssh_private_key_file" => "./.vagrant/machines/#{inventory_name}/virtualbox/private_key",
+        "ansible_host" => network['ip_address'],
+        "ansible_ssh_private_key_file" => "./.vagrant/machines/#{network['inventory_name']}/virtualbox/private_key",
 
         "vars" => {
             "vault_cluster_name" => datacenter,
             "vault_datacenter" => datacenter,
-            "vault_address" => ip_address,
-            "vault_cluster_addr" => "http://#{hostname}:{{ (vault_port | int) + 1 }}",
-            "vault_api_addr" => "http://#{hostname}:{{ vault_port }}"
+            "vault_address" => network['ip_address'],
+            "vault_cluster_address" => "{{ vault_address }}:{{ (vault_port | int) + 1 }}",
+            "vault_cluster_addr" => "http://#{network['hostname']}:{{ (vault_port | int) + 1 }}",
+            "vault_api_addr" => "http://#{network['hostname']}:{{ vault_port }}"
+        }
+    }
+    elsif (is_consul_bootstrap(service, index) && !datacenter_config.key?('vault'))
+    {
+        "host" => network['hostname'],
+        "ip_addr" => network['ip_address'],
+        "inventory_name" => network['inventory_name'],
+
+        "ansible_host" => network['ip_address'],
+        "ansible_ssh_private_key_file" => "./.vagrant/machines/#{network['inventory_name']}/virtualbox/private_key",
+
+        "vars" => {
+            "vault_cluster_name" => datacenter,
+            "vault_datacenter" => datacenter,
+            "vault_address" => network['ip_address'],
+            "vault_cluster_address" => "{{ vault_address }}:{{ (vault_port | int) + 1 }}",
+            "vault_cluster_addr" => "http://#{network['hostname']}:{{ (vault_port | int) + 1 }}",
+            "vault_api_addr" => "http://#{network['hostname']}:{{ vault_port }}"
         }
     }
     end
@@ -201,37 +273,42 @@ end
 
 def make_nomad_service(datacenter, datacenter_config, service, service_config, index)
 
-    cidr_ii = datacenter_config["cidr_segment"]
-
-    hostname = make_host_name(datacenter, service, index)
-    ip_address = make_private_ip(cidr_ii, service_config, index)
-    inventory_name = make_inventory_name(datacenter, service, index)
-
+    network = make_network(datacenter, datacenter_config, service, service_config, index)
     
-    nomad_role = if service == 'nomad_server'
-        'server'
-    elsif service == 'nomad_client'
-        'client'
-    elsif (!datacenter_config.key?('nomad_server') && service == 'consul')
-        'server'
-    end
+    bootstrap = {
+        'role' => if service == 'nomad_server'
+            'server'
+        elsif service == 'nomad_client'
+            'client'
+        elsif (!datacenter_config.key?('nomad_server') && service == 'consul')
+            if (!datacenter_config.key?('nomad_client'))
+                'both'
+            else
+                'server'
+            end
+        end,
+        'expect' =>  if (!datacenter_config.key?('nomad_server') && service == 'consul')
+            datacenter_config['consul']['count']
+        else
+            '{{ nomad_servers | count }}'
+        end
+    }
 
-    if !(nomad_role == nil) 
+    if !(bootstrap['role'] == nil) 
     {
-        "host" => hostname,
-        "ip_addr" => ip_address,
-        "inventory_name" => inventory_name,
+        "host" => network['hostname'],
+        "ip_addr" => network['ip_address'],
+        "inventory_name" => network['inventory_name'],
 
-        "ansible_host" => ip_address,
-        "ansible_ssh_private_key_file" => "./.vagrant/machines/#{inventory_name}/virtualbox/private_key",
+        "ansible_host" => network['ip_address'],
+        "ansible_ssh_private_key_file" => "./.vagrant/machines/#{network['inventory_name']}/virtualbox/private_key",
 
         "vars" => {
-            "nomad_node_role" => nomad_role,
+            "nomad_node_role" => bootstrap['role'],
+            "nomad_bootstrap_expect" => bootstrap['expect'],
             "nomad_datacenter" => datacenter,
-            "nomad_bind_address" => ip_address,
-            "nomad_advertise_address" => ip_address,
-            "nomad_consul_address" => "#{ip_address}:8500",
-            "nomad_vault_address" => make_host_name(datacenter, 'vault', 1)
+            "nomad_bind_address" => network['ip_address'],
+            "nomad_advertise_address" => network['ip_address'],
         }
     }
     end
@@ -239,24 +316,38 @@ end
 
 def make_traefik_service(datacenter, datacenter_config, service, service_config, index)
 
-    cidr_ii = datacenter_config["cidr_segment"]
-
-    hostname = make_host_name(datacenter, service, index)
-    ip_address = make_private_ip(cidr_ii, service_config, index)
-    inventory_name = make_inventory_name(datacenter, service, index)
+    network = make_network(datacenter, datacenter_config, service, service_config, index)
     
-    if service == 'traefik'
+    if (service == 'traefik' || (is_consul_bootstrap(service, index) && !datacenter_config.key?('traefik')))
     {
-        "host" => hostname,
-        "ip_addr" => ip_address,
-        "inventory_name" => inventory_name,
+        "host" => network['hostname'],
+        "ip_addr" => network['ip_address'],
+        "inventory_name" => network['inventory_name'],
 
-        "ansible_host" => ip_address,
-        "ansible_ssh_private_key_file" => "./.vagrant/machines/#{inventory_name}/virtualbox/private_key",
+        "ansible_host" => network['ip_address'],
+        "ansible_ssh_private_key_file" => "./.vagrant/machines/#{network['inventory_name']}/virtualbox/private_key",
 
         "vars" => {
-            "consul_address" => ip_address
+            'consul_address' => network['ip_address']
         }
+    }
+    end
+end
+
+def make_registry_service(datacenter, datacenter_config, service, service_config, index)
+
+    network = make_network(datacenter, datacenter_config, service, service_config, index)
+    
+    if (service == 'registry' || (is_consul_bootstrap(service, index) && !datacenter_config.key?('registry')))
+    {
+        "host" => network['hostname'],
+        "ip_addr" => network['ip_address'],
+        "inventory_name" => network['inventory_name'],
+
+        "ansible_host" => network['ip_address'],
+        "ansible_ssh_private_key_file" => "./.vagrant/machines/#{network['inventory_name']}/virtualbox/private_key",
+
+        "vars" => {}
     }
     end
 end
